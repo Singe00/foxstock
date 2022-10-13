0057487f438fdc982f16cbc8ae376f822d36e1a8
GyeongHo <ludeng0727@gmail.com>
2022-10-13 21:08:29 +0900

    TEST CodeAPITest 수정

--- src/test/java/kumoh/opensource/foxstock/api/CodeAPITest.java
+++ src/test/java/kumoh/opensource/foxstock/api/CodeAPITest.java
@@ -1,13 +1,16 @@
 package kumoh.opensource.foxstock.api;
 
+import kumoh.opensource.foxstock.api.dto.CodeDto;
 import org.json.simple.parser.ParseException;
 import org.junit.jupiter.api.Test;
 import org.springframework.http.ResponseEntity;
 
 import java.io.IOException;
+import java.util.List;
 import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.*;
+
 
 class CodeAPITest {
 
@@ -15,8 +18,12 @@ class CodeAPITest {
     void getCodesTest() throws IOException, ParseException {
         CodeAPI codeAPI = new CodeAPI();
 
-        codeAPI.getCodes();
+        List<CodeDto> codes = codeAPI.getCodes();
 
+        for (CodeDto cor :
+                codes) {
+            System.out.println(codes.indexOf(cor) + ":" + cor);
+        }
 
     }
 
