bfd752e6089d372716a50e323643e346f6660f92
Singery <singery00@naver.com>
2022-11-10 16:32:58 +0900

    Find ID/PW

--- build/resources/main/static/sql/create_user.sql
+++ build/resources/main/static/sql/create_user.sql
@@ -1,7 +1,10 @@
 create table user(
     id int auto_increment,
+    user_name varchar(32),
     user_id varchar(32),
     password varchar(32),
+    user_check_question_number int,
+    user_check_question_answer varchar(32),
     primary key (id)
 );
 
